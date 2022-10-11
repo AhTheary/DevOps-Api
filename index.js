be7e29f1b3138117a85fcc2e635841d1e4a5616b
AhTheary <thearykheary@gmail.com>
2022-10-11 22:07:58 +0700

    v3

--- index.js
+++ index.js
@@ -43,4 +43,4 @@ app.use((err, req, res, next) => {
 })
 
 
-app.listen(process.env.PORT || 3001, () => console.log('App avaiable on http://50.19.156.48:3001'))
+app.listen(process.env.PORT || 3001, () => console.log('App avaiable on http://184.72.113.158:3001'))
